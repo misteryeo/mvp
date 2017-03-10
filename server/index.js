66a02535b35ba20aefe31b1f7072cbdaf12a6645
Andy Yeo <andy.yeo@gmail.com>
2017-03-10 12:27:40 -0800

    Add POST response.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
 var http = require('http');
+var bodyParser = require('body-parser')
 var express = require('express');
 var app = express();
 
+var line = ['If you joined a circus what circus act would you be?'];
+
 app.get('/', function(req, res) {
-  res.send('Hello world!')
+  res.writeHead(200, {
+    'content-type': 'application/json'
+  });
+  //res.send and res.end difference?
+  res.end(JSON.stringify(line));
 })
+
+app.post('/', function(req, res) {
+  var body = [];
+  res.on('data', function(chunk) {
+    body.push(chunk);
+  }).on('end', function() {
+    console.log(body);
+    body = Buffer.concat(body).toString();
+    line.push(JSON.parse(body));
+  });
+})
+
 
 app.listen(3000, function() {
   console.log('Express server is running!')
