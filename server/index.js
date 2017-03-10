a6253e79d7bd7e646bbee894c020597f4ecd717e
Andy Yeo <andy.yeo@gmail.com>
2017-03-10 15:26:08 -0800

    Render HTML to server.

--- server/index.js
+++ server/index.js
@@ -1,30 +1,57 @@
 var http = require('http');
-var bodyParser = require('body-parser')
+var sql = require('mysql');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var express = require('express');
 var app = express();
+var path = require('path');
+
+var port = process.env.PORT || 3000;
 
 var line = ['If you joined a circus what circus act would you be?'];
 
+//app.use(bodyParser.json())
+// Serves the static files from client side HTML, JS, CSS, images
+app.use(express.static(__dirname + '/public'))
+//app.use(cookieParser);
+
 app.get('/', function(req, res) {
-  res.writeHead(200, {
-    'content-type': 'application/json'
-  });
-  //res.send and res.end difference?
-  res.end(JSON.stringify(line));
-})
+var fileName = path.join(__dirname+'/../client/index.html');
+res.sendFile(fileName);
+});
+
+// app.get('/', function(req, res) {
+//   var options = {
+//     headers: {
+//       'content-type': 'application/json'
+//     }
+//   };
+
+// var fileName = path.join(__dirname+'/../client/index.html');
+
+// res.sendFile(fileName, options, function(err) {
+//   if(err) {
+//     console.error(err);
+//   } else {
+//     res.status(200).send('All good!')
+//   }
+// });
+// //res.send and res.end difference?
+// // res.end(JSON.stringify(line));
+// });
 
 app.post('/', function(req, res) {
   var body = [];
-  res.on('data', function(chunk) {
+  req.on('data', function(chunk) {
     body.push(chunk);
   }).on('end', function() {
-    console.log(body);
     body = Buffer.concat(body).toString();
     line.push(JSON.parse(body));
   });
+  res.end();
 })
 
 
-app.listen(3000, function() {
-  console.log('Express server is running!')
+app.listen(port, function() {
+  console.log('Express server is running and listening on port: ' + port);
 })
