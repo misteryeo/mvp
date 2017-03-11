e4c1ac91b6f448984f600979eeb14a46287f4960
Andy Yeo <andy.yeo@gmail.com>
2017-03-10 18:12:12 -0800

    Rectify minor bugs.

--- server/index.js
+++ server/index.js
@@ -1,43 +1,46 @@
 var http = require('http');
-var sql = require('mysql');
+var mysql = require('mysql');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var express = require('express');
-var app = express();
+var cors = require('cors');
 var path = require('path');
+
+
+var app = express();
+app.use(cors());
 
 var port = process.env.PORT || 3000;
 
 var line = ['If you joined a circus what circus act would you be?'];
 
+var connection = mysql.createConnection({
+  host: 'http://127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'icebreaker'
+})
+
 //app.use(bodyParser.json())
 // Serves the static files from client side HTML, JS, CSS, images
-app.use(express.static(__dirname + '/public'))
+app.use(express.static('public'));
+//app.use('/scripts', express.static('node_modules/angular'));
 //app.use(cookieParser);
 
 app.get('/', function(req, res) {
-var fileName = path.join(__dirname+'/../client/index.html');
-res.sendFile(fileName);
+
+  connection.query('SELECT oneline FROM convolines ORDER BY RANDOM() LIMIT 1', function(error, results, fields){
+    console.log('results', results);
+  });
+
 });
 
 // app.get('/', function(req, res) {
-//   var options = {
-//     headers: {
-//       'content-type': 'application/json'
-//     }
-//   };
+  
+//   var fileName = path.join(__dirname+'/../client/index.html');
 
-// var fileName = path.join(__dirname+'/../client/index.html');
+//   res.sendFile(fileName);
 
-// res.sendFile(fileName, options, function(err) {
-//   if(err) {
-//     console.error(err);
-//   } else {
-//     res.status(200).send('All good!')
-//   }
-// });
-// //res.send and res.end difference?
-// // res.end(JSON.stringify(line));
 // });
 
 app.post('/', function(req, res) {
@@ -48,7 +51,7 @@ app.post('/', function(req, res) {
     body = Buffer.concat(body).toString();
     line.push(JSON.parse(body));
   });
-  res.end();
+  res.send();
 })
 
 
