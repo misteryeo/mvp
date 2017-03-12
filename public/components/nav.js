7152436edd6f1192639b7a42b85dd2aad19ec549
Andy Yeo <andy.yeo@gmail.com>
2017-03-11 16:38:48 -0800

    Saving progress on buggy AngularJS!

--- public/components/nav.js
+++ public/components/nav.js
@@ -1,18 +1,18 @@
 angular.module('MyApp')
 
-.controller('navController', function($scope) {
+.controller('navController', function() {
 
-  $scope.greeting = 'Have you ever been stuck wondering how to say something interesting to that new person you just met?';
+  this.greeting = 'Have you ever been stuck wondering how to say something interesting to that new person you just met?';
   
-  $scope.firstClick = function() {
-    $scope.greeting = 'Do you find yourself always being asked where you\'re from or where you work?';
+  this.firstClick = function() {
+    this.greeting = 'Do you find yourself always being asked where you\'re from or where you work?';
   };
 
-  $scope.secondClick = function() {
-    $scope.greeting = 'Ever wondered how dinner conversations could be more interesting?'
+  this.secondClick = function() {
+    this.greeting = 'Ever wondered how dinner conversations could be more interesting?'
   };
 
-  $scope.CTA = 'Click on the button to try!'
+  this.CTA = 'Click on the button to try!'
 
 })
 
